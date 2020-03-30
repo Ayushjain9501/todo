9bcaaae2172280b8bd8da2079fac716c50f2fde4
Ayush Jain <ayushjain9501@gmail.com>
2020-03-30 20:25:31 +0530

    upload files

---
+++ pytorch_nlp/generate/generate.py
@@ -0,0 +1,214 @@
+# train character level RNN to generate surnames over 18 languages of origing
+from io import open
+import glob
+import os
+
+import unicodedata
+import string
+
+import torch
+import torch.nn as nn
+
+import random
+import time
+import math
+
+import matplotlib.pyplot as plt 
+import matplotlib.ticker as ticker
+
+
+def findFiles(path): return glob.glob(path)
+
+
+# print(findFiles('data/names/*.txt'))
+all_letters = string.ascii_letters + "./;'-"
+n_letters = len(all_letters) + 1 #<EOS>
+# print(all_letters)
+
+
+# Ślusàrski -> Slusarski
+def unicodeToAscii(s):
+    return ''.join(c for c in unicodedata.normalize('NFD', s) if unicodedata.category(c) != 'Mn' and c in all_letters)
+
+
+# print(unicodeToAscii('Ślusàrski'))
+category_lines = {}
+all_categories = []
+
+
+def readLines(filename):
+    lines = open(filename, encoding='utf-8').read().strip().split('\n')
+    return [unicodeToAscii(line) for line in lines]
+
+
+for filename in findFiles('data/names/*.txt'):
+    category = os.path.splitext(os.path.basename(filename))[0]
+    all_categories.append(category)
+    lines = readLines(filename)
+    category_lines[category] = lines
+
+n_categories = len(all_categories)
+
+
+# define the nn
+class RNN(nn.Module):
+    def __init__(self, input_size, hidden_size, output_size):
+        super(RNN, self).__init__()
+
+        self.hidden_size = hidden_size
+
+        self.i2h = nn.Linear(n_categories + input_size + hidden_size, hidden_size)
+        self.i20 = nn.Linear(n_categories + input_size + hidden_size, output_size)
+        self.o2o = nn.Linear(hidden_size + output_size, output_size)
+        self.dropout = nn.Dropout(0.1)
+        self.softmax = nn.LogSoftmax(dim=1)
+
+    def forward(self, category, input, hidden):
+        input_combined = torch.cat((category, input, hidden), 1)
+        hidden = self.i2h(input_combined)
+        output = self.i20(input_combined)
+        output_combined = torch.cat((hidden, output), 1)
+        output = self.o2o(output_combined)
+        output = self.dropout(output)
+        output = self.softmax(output)
+        return output, hidden
+        
+    def initHidden(self):
+        return torch.zeros(1, self.hidden_size)
+
+
+def randomChoice(l):
+    return l[random.randint(0,len(l)-1)]
+
+def randomTrainingPair():
+    category = randomChoice(all_categories)
+    line = randomChoice(category_lines[category])
+    return category, line
+
+# for i in range(10) :
+#     category, line, category_tensor, line_tensor = randomTrainingExample()
+#     print('category =', category, '/ line = ', line)
+#     print(category_tensor)
+
+#creating i/o tensors
+def categoryTensor(category):
+    li = all_categories.index(category)
+    tensor = torch.zeros(1,n_categories)
+    tensor[0][li] = 1
+    return tensor
+
+
+def inputTensor(line) :
+    tensor = torch.zeros(len(line),1, n_letters)
+    for li in range(len(line)):
+        letter = line[li]
+        tensor[li][0][all_letters.find(letter)] = 1
+    return tensor
+
+def targetTensor(line):
+    letter_indexes = [all_letters.find(line[li]) for li in range(1,len(line))]
+    letter_indexes.append(n_letters - 1) #<EOS>
+    return torch.LongTensor(letter_indexes)
+
+# Random Training Example
+def randomTrainingExample():
+    category, line = randomTrainingPair()
+    category_tensor = categoryTensor(category)
+    input_line_tensor = inputTensor(line)
+    target_line_tensor = targetTensor(line)
+    return category_tensor, input_line_tensor, target_line_tensor
+
+
+criterion = nn.NLLLoss()
+learning_rate = 0.005
+
+def train(category_tensor, input_line_tensor, target_line_tensor):
+    target_line_tensor.unsqueeze_(-1)
+    hidden = rnn.initHidden()
+
+    rnn.zero_grad()
+
+    loss = 0
+
+    for i in range(input_line_tensor.size()[0]):
+        output, hidden = rnn(category_tensor, input_line_tensor[i], hidden)
+        l = criterion(output, target_line_tensor[i])
+        loss += l
+    loss.backward()
+
+    for p in rnn.parameters():
+        p.data.add_(-learning_rate, p.grad.data)
+    
+    return output, loss.item() / input_line_tensor.size()[0]
+
+def timeSince(since):
+    now = time.time()
+    s = now-since
+    m = math.floor(s/60)
+    s -= m*60
+    return "%dm %ds" %(m ,s)
+
+
+rnn = RNN(n_letters, 128, n_letters)
+
+n_iters = 100000
+print_every = 5000
+plot_every = 500
+total_loss = 0
+all_losses = []
+
+start = time.time()
+
+for iter in range(1, n_iters + 1):
+    category_tensor, input_line_tensor, target_line_tensor = randomTrainingExample()
+    output, loss = train(category_tensor, input_line_tensor, target_line_tensor)
+    total_loss += loss
+
+    if iter % print_every == 0:
+        print('%s (%d %d%%) %.4f' % (timeSince(start), iter, iter / n_iters * 100, loss))
+
+    if iter % plot_every == 0:
+        all_losses.append(total_loss / plot_every)
+        total_loss = 0
+
+
+plt.figure()
+plt.plot(all_losses)
+plt.show()
+
+max_length = 20
+
+
+def sample(category, start_letter='A'):
+    with torch.no_grad():  
+        category_tensor = categoryTensor(category)
+        input = inputTensor(start_letter)
+        hidden = rnn.initHidden()
+
+        output_name = start_letter
+
+        for i in range(max_length):
+            output, hidden = rnn(category_tensor, input[0], hidden)
+            topv, topi = output.topk(1)
+            topi = topi[0][0]
+            if topi == n_letters - 1:
+                break
+            else:
+                letter = all_letters[topi]
+                output_name += letter
+            input = inputTensor(letter)
+
+        return output_name
+
+
+def samples(category, start_letters='ABC'):
+    for start_letter in start_letters:
+        print(sample(category, start_letter))
+
+samples('Russian', 'RUS')
+
+samples('German', 'GER')
+
+samples('Spanish', 'SPA')
+
+samples('Chinese', 'CHI')
